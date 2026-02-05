2b4885bf28f56240b91d34fcb867c6734dff8f25
DELL 3400 <DELL 3400@DESKTOP-Q4C4NUA>
2026-02-05 11:54:12 -0800

    Updated Code

---
+++ A6-Java-Selenium/src/org/demoPackage/LaunchChromeBrowser.java
@@ -0,0 +1,16 @@
+package org.demoPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChromeBrowser {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		 WebDriver driver=new ChromeDriver();
+		 Thread.sleep(4000);
+		 driver.close();
+
+	}
+
+}
